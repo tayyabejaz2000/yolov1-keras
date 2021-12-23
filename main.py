3438afbbfc61bb575295aa4a4257d987ebb15814
tayyabejaz2000 <43398844+tayyabejaz2000@users.noreply.github.com>
2021-12-23 23:20:53 +0500

    Updated main.py

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
+import os
+
+from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.optimizers import Adam
 
 from read_data import BatchedGenerator
 from yolo import loss, model
 
+model_checkpoint = ModelCheckpoint(
+    'model.h5',
+    save_weights_only=True
+)
+
+
 network = model.GetModel((448, 448, 3))
 
-network.compile(optimizer=Adam(learning_rate=1e-4), loss=loss.YOLOLoss())
+network.compile(
+    optimizer=Adam(learning_rate=1e-4),
+    loss=loss.YOLOLoss()
+)
 
-for batch_x, batch_y in BatchedGenerator(batch_size=256):
-    network.fit(batch_x, batch_y, epochs=2,
-                batch_size=16, verbose=1)
+if 'model.h5' in os.listdir(os.getcwd()):
+    network.load_weights('model.h5')
+
+for batch_x, batch_y in BatchedGenerator(batch_size=512):
+    network.fit(
+        batch_x,
+        batch_y,
+        epochs=2,
+        batch_size=32,
+        callbacks=[model_checkpoint]
+    )
